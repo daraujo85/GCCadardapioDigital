49f8c5f6533a11f2daaf424ad7c5cc8e0d16d30e
brenno caldas <brennodevelop100@gmail.com>
2022-08-10 19:57:00 -0300

    Criando a classe User

---
+++ Backend/Squirrel/Squirrel.Domain/Domains/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Squirrel.Domain.Domains
+{
+    internal class EntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ Backend/Squirrel/Squirrel.Domain/Domains/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Squirrel.Domain.Domains
+{
+    internal class User : EntityBase
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        // Esperando a relação entre usuario e empresa
+    }
+}
