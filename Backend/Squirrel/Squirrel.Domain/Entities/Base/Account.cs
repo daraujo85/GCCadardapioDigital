68a5448e5793c879f13878280cc16e194563110e
brenno caldas <brennodevelop100@gmail.com>
2022-08-12 19:57:04 -0300

    I added the account class and the enumerated permissionTypeEmun

---
+++ Backend/Squirrel/Squirrel.Domain/Entities/Base/Account.cs
@@ -0,0 +1,13 @@
+﻿using Squirrel.Domain.Enums;
+
+namespace Squirrel.Domain.Entities.Base
+{
+    internal class Account : EntityBase
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public PermissionTypeEnum Permission { get; set; }
+        public Guid IdUser { get; set; }
+        public User User { get; set; }
+    }
+}
--- Backend/Squirrel/Squirrel.Domain/Entities/Base/User.cs
+++ Backend/Squirrel/Squirrel.Domain/Entities/Base/User.cs
@@ -6,6 +6,8 @@
         public string LastName { get; set; }
         public string Email { get; set; }
 
+        public virtual Account Account { get; set; }
+
         // Esperando a relação entre usuario e empresa
     }
 }
