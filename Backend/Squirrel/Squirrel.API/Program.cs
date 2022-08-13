a3deb157393b1d04296cad5d7e03bfedcef22821
Sandro Brito <san_juq@hotmail.com>
2022-08-12 23:27:32 -0300

    Configurando-classe-contexto

--- Backend/Squirrel/Squirrel.API/Program.cs
+++ Backend/Squirrel/Squirrel.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Squirrel.Infra.Data.Context;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CardapioDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("SquirrelConnection"));
+});
 
 var app = builder.Build();
 
---
+++ Backend/Squirrel/Squirrel.Infra.Data/Context/CardapioDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squirrel.Infra.Data.Context
+{
+    public class CardapioDbContext : DbContext
+    {
+        public CardapioDbContext(DbContextOptions<CardapioDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
