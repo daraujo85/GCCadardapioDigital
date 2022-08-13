56901c3a58cd508b58a6defce3066ae660c7fa74
Moises Alvarenga <moises.alvarenga@outlook.com>
2022-08-13 14:53:20 -0300

    Foi criado o repositório genérico e a interface para encapsulamento da mesma

---
+++ Backend/Squirrel/Squirrel.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using Squirrel.Domain.Entities.Base;
+using System.Linq.Expressions;
+
+namespace Squirrel.Domain.Interfaces
+{
+    public interface IRepository<TEntity> :IDisposable where TEntity : EntityBase
+    {
+        Task Insert(TEntity entity);
+        Task<TEntity> GetId(Guid id);
+        Task<List<TEntity>> GetAll();
+        Task Update(TEntity entity);
+        Task Delete(Guid id);
+
+
+        /*Esse método usa uma expressão lambida para fazer buscas específicas
+         * 
+            Exemplo: Get(x => x.Name);         
+         */
+        Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> expression);
+    }
+}
---
+++ Backend/Squirrel/Squirrel.Infra.Data/Repository/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Squirrel.Domain.Entities.Base;
+using Squirrel.Domain.Interfaces;
+using Squirrel.Infra.Data.Context;
+using System.Linq.Expressions;
+
+
+namespace Squirrel.Infra.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : EntityBase, new()
+    {
+        protected readonly CardapioDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        public Repository(CardapioDbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntity>();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> expression)
+        {
+            return await DbSet.AsNoTracking().Where(expression).ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetId(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<TEntity>> GetAll()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task Insert(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await Db.SaveChangesAsync();
+        }
+
+        public virtual async Task Update(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await Db.SaveChangesAsync();
+        }
+
+        public virtual async Task Delete(Guid id)
+        {
+            DbSet.Remove(new TEntity { Id = id });
+            await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+    }
+}
