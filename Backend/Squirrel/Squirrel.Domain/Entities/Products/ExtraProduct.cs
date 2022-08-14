843fdf088f17565614bd70a2a7f7a8de7cbef34c
Gabriel Moya <gabriel.mm@lyncas.net>
2022-08-14 14:43:42 -0300

    feat: create class ExtraOptions and ExtraProduct

---
+++ Backend/Squirrel/Squirrel.Domain/Entities/Products/ExtraProduct.cs
@@ -0,0 +1,91 @@
+﻿using Squirrel.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Squirrel.Domain.Entities.Products
+{
+    public class ExtraProduct : EntityBase
+    {
+        public ExtraProduct() { }
+
+        public ExtraProduct(string extrasTitle,
+                            int limitChoicesHowMany,
+                            bool required,
+                            bool available,
+                            string description,
+                            List<ExtraOptions> extraOptions)
+        {
+            ExtrasTitle = extrasTitle;
+            LimitChoicesHowMany = limitChoicesHowMany;
+            Required = required;
+            Available = available;
+            Description = description;
+            ExtraOptions = extraOptions;
+        }
+
+
+        public string ExtrasTitle { get; private set; }
+        public int LimitChoicesHowMany { get; private set; } = 0;
+        public bool Required { get; private set; }
+        public bool Available { get; private set; }
+        public string Description { get; private set; }
+        public List<ExtraOptions> ExtraOptions { get; private set; }
+
+
+        public void Update(ExtraProduct extraProduct)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(extraProduct));
+
+            ExtrasTitle = extraProduct.ExtrasTitle;
+            LimitChoicesHowMany = extraProduct.LimitChoicesHowMany;
+            Required = extraProduct.Required;
+            Available = extraProduct.Available;
+            Description = extraProduct.Description;
+            ExtraOptions = extraProduct.ExtraOptions;
+        }
+
+        public void ChangeExtrasTitle(string extrasTitle)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(extrasTitle));
+            ExtrasTitle = extrasTitle;
+        }
+
+        public void ChangeLimitChoices(int limitChoices)
+        {
+            LimitChoicesHowMany = limitChoices;
+        }
+
+        public void ChangeRequired(bool required)
+        {
+            Required = required;
+        }
+
+        public void ChangeAvailable(bool available)
+        {
+            Available = available;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            Description = description;
+        }
+
+        public void AddExtraOption(ExtraOptions extraOptions)
+        {
+            ExtraOptions ??= new List<ExtraOptions>();
+            ExtraOptions.Add(extraOptions);
+        }
+
+        public void RemoveExtraOption(Guid id)
+        {
+            if (!ExtraOptions.Any()) return;
+
+            var index = ExtraOptions.FindIndex(x => x.Id == id);
+            ExtraOptions.RemoveAt(index);
+        }
+    }
+}
