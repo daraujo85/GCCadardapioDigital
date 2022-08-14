2405cc677aeff9e5761f68ae53bbeb5cebe70a8b
Gabriel Moya <gabriel.mm@lyncas.net>
2022-08-14 14:44:57 -0300

    chore: remove keys from namespaces

--- Backend/Squirrel/Squirrel.Domain/Entities/Products/ExtraOptions.cs
+++ Backend/Squirrel/Squirrel.Domain/Entities/Products/ExtraOptions.cs
@@ -1,58 +1,57 @@
 ﻿using Squirrel.Domain.Entities.Base;
 
-namespace Squirrel.Domain.Entities.Products
+namespace Squirrel.Domain.Entities.Products;
+
+public class ExtraOptions : EntityBase
 {
-    public class ExtraOptions : EntityBase
+    public ExtraOptions() { }
+
+    public ExtraOptions(string name,
+                        decimal price,
+                        string description,
+                        bool available)
     {
-        public ExtraOptions() { }
-
-        public ExtraOptions(string name,
-                            decimal price,
-                            string description,
-                            bool available)
-        {
-            Name = name;
-            Price = price;
-            Description = description;
-            Available = available;
-        }
+        Name = name;
+        Price = price;
+        Description = description;
+        Available = available;
+    }
 
 
-        public string Name { get; private set; }
-        public decimal Price { get; private set; }
-        public string Description { get; private set; }
-        public bool Available { get; private set; }
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+    public string Description { get; private set; }
+    public bool Available { get; private set; }
 
 
-        public void Update(ExtraOptions extraOptions)
-        {
-            ArgumentNullException.ThrowIfNull(nameof(extraOptions));
+    public void Update(ExtraOptions extraOptions)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(extraOptions));
 
-            Name = extraOptions.Name;
-            Price = extraOptions.Price;
-            Description = extraOptions.Description;
-            Available = extraOptions.Available;
-        }
+        Name = extraOptions.Name;
+        Price = extraOptions.Price;
+        Description = extraOptions.Description;
+        Available = extraOptions.Available;
+    }
 
-        public void ChangeName(string name)
-        {
-            ArgumentNullException.ThrowIfNull(nameof(name));
-            Name = name;
-        }
+    public void ChangeName(string name)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(name));
+        Name = name;
+    }
 
-        public void ChangePrice(decimal price)
-        {
-            Price = price;
-        }
+    public void ChangePrice(decimal price)
+    {
+        Price = price;
+    }
 
-        public void ChangeDescription(string description)
-        {
-            Description = description;
-        }
+    public void ChangeDescription(string description)
+    {
+        Description = description;
+    }
 
-        public void ChangeAvailable(bool available)
-        {
-            Available = available;
-        }
+    public void ChangeAvailable(bool available)
+    {
+        Available = available;
     }
 }
--- Backend/Squirrel/Squirrel.Domain/Entities/Products/ExtraProduct.cs
+++ Backend/Squirrel/Squirrel.Domain/Entities/Products/ExtraProduct.cs
@@ -1,85 +1,84 @@
 ﻿using Squirrel.Domain.Entities.Base;
 
-namespace Squirrel.Domain.Entities.Products
+namespace Squirrel.Domain.Entities.Products;
+
+public class ExtraProduct : EntityBase
 {
-    public class ExtraProduct : EntityBase
+    public ExtraProduct() { }
+
+    public ExtraProduct(string extrasTitle,
+                        int limitChoicesHowMany,
+                        bool required,
+                        bool available,
+                        string description,
+                        List<ExtraOptions> extraOptions)
     {
-        public ExtraProduct() { }
-
-        public ExtraProduct(string extrasTitle,
-                            int limitChoicesHowMany,
-                            bool required,
-                            bool available,
-                            string description,
-                            List<ExtraOptions> extraOptions)
-        {
-            ExtrasTitle = extrasTitle;
-            LimitChoicesHowMany = limitChoicesHowMany;
-            Required = required;
-            Available = available;
-            Description = description;
-            ExtraOptions = extraOptions;
-        }
+        ExtrasTitle = extrasTitle;
+        LimitChoicesHowMany = limitChoicesHowMany;
+        Required = required;
+        Available = available;
+        Description = description;
+        ExtraOptions = extraOptions;
+    }
 
 
-        public string ExtrasTitle { get; private set; }
-        public int LimitChoicesHowMany { get; private set; } = 0;
-        public bool Required { get; private set; }
-        public bool Available { get; private set; }
-        public string Description { get; private set; }
-        public List<ExtraOptions> ExtraOptions { get; private set; }
+    public string ExtrasTitle { get; private set; }
+    public int LimitChoicesHowMany { get; private set; } = 0;
+    public bool Required { get; private set; }
+    public bool Available { get; private set; }
+    public string Description { get; private set; }
+    public List<ExtraOptions> ExtraOptions { get; private set; }
 
 
-        public void Update(ExtraProduct extraProduct)
-        {
-            ArgumentNullException.ThrowIfNull(nameof(extraProduct));
+    public void Update(ExtraProduct extraProduct)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(extraProduct));
 
-            ExtrasTitle = extraProduct.ExtrasTitle;
-            LimitChoicesHowMany = extraProduct.LimitChoicesHowMany;
-            Required = extraProduct.Required;
-            Available = extraProduct.Available;
-            Description = extraProduct.Description;
-            ExtraOptions = extraProduct.ExtraOptions;
-        }
+        ExtrasTitle = extraProduct.ExtrasTitle;
+        LimitChoicesHowMany = extraProduct.LimitChoicesHowMany;
+        Required = extraProduct.Required;
+        Available = extraProduct.Available;
+        Description = extraProduct.Description;
+        ExtraOptions = extraProduct.ExtraOptions;
+    }
 
-        public void ChangeExtrasTitle(string extrasTitle)
-        {
-            ArgumentNullException.ThrowIfNull(nameof(extrasTitle));
-            ExtrasTitle = extrasTitle;
-        }
+    public void ChangeExtrasTitle(string extrasTitle)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(extrasTitle));
+        ExtrasTitle = extrasTitle;
+    }
 
-        public void ChangeLimitChoices(int limitChoices)
-        {
-            LimitChoicesHowMany = limitChoices;
-        }
+    public void ChangeLimitChoices(int limitChoices)
+    {
+        LimitChoicesHowMany = limitChoices;
+    }
 
-        public void ChangeRequired(bool required)
-        {
-            Required = required;
-        }
+    public void ChangeRequired(bool required)
+    {
+        Required = required;
+    }
 
-        public void ChangeAvailable(bool available)
-        {
-            Available = available;
-        }
+    public void ChangeAvailable(bool available)
+    {
+        Available = available;
+    }
 
-        public void ChangeDescription(string description)
-        {
-            Description = description;
-        }
+    public void ChangeDescription(string description)
+    {
+        Description = description;
+    }
 
-        public void AddExtraOption(ExtraOptions extraOptions)
-        {
-            ExtraOptions ??= new List<ExtraOptions>();
-            ExtraOptions.Add(extraOptions);
-        }
+    public void AddExtraOption(ExtraOptions extraOptions)
+    {
+        ExtraOptions ??= new List<ExtraOptions>();
+        ExtraOptions.Add(extraOptions);
+    }
 
-        public void RemoveExtraOption(Guid id)
-        {
-            if (!ExtraOptions.Any()) return;
+    public void RemoveExtraOption(Guid id)
+    {
+        if (!ExtraOptions.Any()) return;
 
-            var index = ExtraOptions.FindIndex(x => x.Id == id);
-            ExtraOptions.RemoveAt(index);
-        }
+        var index = ExtraOptions.FindIndex(x => x.Id == id);
+        ExtraOptions.RemoveAt(index);
     }
 }
