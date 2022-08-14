c04289d75839e76cc8e588f80dbadf1a8fdae41f
Gabriel Moya <gabriel.mm@lyncas.net>
2022-08-14 14:44:25 -0300

    chore: remove unnecessary usings

--- Backend/Squirrel/Squirrel.Domain/Entities/Products/ExtraProduct.cs
+++ Backend/Squirrel/Squirrel.Domain/Entities/Products/ExtraProduct.cs
@@ -1,10 +1,4 @@
 ﻿using Squirrel.Domain.Entities.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace Squirrel.Domain.Entities.Products
 {
