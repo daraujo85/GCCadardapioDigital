7eccce5bd4c1996c2c100dc12196d7cfa225e291
brenno caldas <brennodevelop100@gmail.com>
2022-08-15 12:17:27 -0300

    I created the Folder Acess and add 2 metodos the change password and change permission in the account class

---
+++ Backend/Squirrel/Squirrel.Domain/Entities/Access/Account.cs
@@ -0,0 +1,38 @@
+﻿using Squirrel.Domain.Entities.Base;
+using Squirrel.Domain.Enums;
+
+namespace Squirrel.Domain.Entities.Access
+{
+    internal class Account : EntityBase
+    {
+        public Account()
+        {
+        }
+
+        public Account(string email, string password, PermissionTypeEnum permission)
+        {
+            Email = email;
+            Password = password;
+            Permission = permission;
+        }
+
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public PermissionTypeEnum Permission { get; private set; }
+
+        public Guid IdUser { get; set; }
+        public User User { get; set; }
+
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+        }
+
+        public void ChangePermission(PermissionTypeEnum permissionTypeEnum)
+        {
+            Permission = permissionTypeEnum;   
+        }
+
+    }
+}
---
+++ Backend/Squirrel/Squirrel.Domain/Entities/Access/User.cs
@@ -0,0 +1,29 @@
+﻿using Squirrel.Domain.Entities.Base;
+
+namespace Squirrel.Domain.Entities.Access
+{
+    internal class User : EntityBase
+    {
+        public User()
+        {
+        }
+
+        public User(string name, string lastName, string email)
+        {
+            Name = name;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public string Name { get; private set; }
+        public string LastName { get; private set; } 
+        public string Email { get; private set; }
+
+
+        public virtual Account Account { get; set; }
+
+        // Esperando a relação entre usuario e empresa
+
+
+    }
+}
