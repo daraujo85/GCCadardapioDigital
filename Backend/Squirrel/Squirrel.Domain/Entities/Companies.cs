6bef9e7e3ef9fd50fcbcee54d91fa0e13a4d9920
Moises <mo3ses404@gmail.com>
2022-08-15 21:16:50 -0300

    Added companie class

---
+++ Backend/Squirrel/Squirrel.Domain/Entities/Companies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squirrel.Domain.Entities
+{
+    public class Companies
+    {
+        public string CNPJ { get; set; }
+        public string CompanyName { get; set; }
+        public string FantasyName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
